639cc801d9f1a71d20b23664fe4899f660216874
wellsilver <wellsilver2016@gmail.com>
2024-09-01 23:36:36 -0400

    red floor

--- examples/opencl.c
+++ examples/opencl.c
@@ -9,17 +9,39 @@ const char *src = R"(
 struct rgba {uchar r,g,b,a;};
 
 __kernel void pixel(__global struct rgba *img, __global unsigned int *width, __global unsigned int *height) {
-  double x = get_global_id(0);
-  double y = get_global_id(1);
-  int i = y*(*width) + x;
+  float ix = get_global_id(0);
+  float iy = get_global_id(1);
+  uint i = iy*(*width) + ix;
 
+  float fov = 90 * (3.14159265/180); // 90 degrees in radians
+  float aspect_ratio = (float) *width / (float) *height; // Assuming square pixels
+
+  // Calculate horizontal angle
+  float horizontal = atan((2 * (ix + 0.5) / (*width) - 1) * tan(fov / 2) * aspect_ratio);
+
+  // Calculate vertical angle
+  float vertical = atan((1 - 2 * (iy + 0.5) / (*height)) * tan(fov / 2));
+
+  vertical -= 0.5;
+
+  // Now you have horizontal and vertical angles, you can use them to calculate ray direction
+  float3 ray_direction = {
+    cos(vertical) * sin(horizontal),
+    sin(vertical),
+    cos(vertical) * cos(horizontal)
+  };
+
+  float3 cam = {500,500,500}; // xyz camera, z=up
+  for (uint dist = 0;dist<255;dist++) {
+    cam += ray_direction;
+    if (cam.y < 400) {img[i].r = dist;return;};
+  }
 };
 )";
 
-
 int main() {
   samImage win = samWindow("opencl test", 480, 480, 0, 0, 0);
-
+  
   // // setup opencl // //
   cl_int err;
   cl_platform_id   platform; // platform owo
@@ -28,7 +50,7 @@ int main() {
   cl_command_queue commandq;      // compute command queue
   cl_program       program;       // compute program
   cl_kernel        compixel;       // compute kernel
-
+  
   err = clGetPlatformIDs(1, &platform, NULL);
   if (err != CL_SUCCESS) return -1;
   err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, NULL);
