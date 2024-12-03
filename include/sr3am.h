94956318c5907a288aa750828b5498582641a986
wellsilver <wellsilver2016@gmail.com>
2024-12-03 11:54:40 -0500

    samKeys enum

--- include/sr3am.h
+++ include/sr3am.h
@@ -29,3 +29,43 @@ extern uint64_t samUpdatePerf(samImage image);
 extern void samFree(void *any);
 // Free's all resources at once
 extern void samCleanup();
+
+enum samKeys {
+  sam_0 = '0',
+  sam_1 = '1',
+  sam_2 = '2',
+  sam_3 = '3',
+  sam_4 = '4',
+  sam_5 = '5',
+  sam_6 = '6',
+  sam_7 = '7',
+  sam_8 = '8',
+  sam_9 = '9',
+	sam_a = 'a',
+	sam_b = 'b',
+	sam_c = 'c',
+	sam_d = 'd',
+	sam_e = 'e',
+	sam_f = 'f',
+	sam_g = 'g',
+	sam_h = 'h',
+	sam_i = 'i',
+	sam_j = 'j',
+	sam_k = 'k',
+	sam_l = 'l',
+	sam_m = 'm',
+	sam_n = 'n',
+	sam_o = 'o',
+	sam_p = 'p',
+	sam_q = 'q',
+	sam_r = 'r',
+	sam_s = 's',
+	sam_t = 't',
+	sam_u = 'u',
+	sam_v = 'v',
+	sam_w = 'w',
+	sam_x = 'x',
+	sam_y = 'y',
+	sam_z = 'z',
+  // TODO: MORE KEYS
+}
